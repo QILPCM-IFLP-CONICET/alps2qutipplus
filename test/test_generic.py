807fb8a6cc2f6f25c6adaffcb4da1362939d0ec4
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2024-11-26 09:26:43 -0300

    More qutip (#25)
    * improving simplify
    * using frozenset for subsystems

--- test/test_generic.py
+++ test/test_generic.py
@@ -2,7 +2,6 @@
 Basic unit test.
 """
 
-
 from alpsqutip.alpsmodels import list_operators_in_alps_xml, model_from_alps_xml
 from alpsqutip.geometry import graph_from_alps_xml, list_graph_in_alps_xml
 from alpsqutip.model import SystemDescriptor
