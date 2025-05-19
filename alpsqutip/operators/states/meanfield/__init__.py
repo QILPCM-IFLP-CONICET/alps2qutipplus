bb1069effdad08bc62c1caec7fed75229ab4d266
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-05-19 07:31:52 -0300

    Split meanfield (#43)
    * split mean field in submodules

---
+++ alpsqutip/operators/states/meanfield/__init__.py
@@ -0,0 +1,13 @@
+from .meanfield import project_meanfield
+from .projections import (
+    one_body_from_qutip_operator,
+    project_operator_to_m_body,
+    project_to_n_body_operator,
+)
+
+ALL = [
+    "one_body_from_qutip_operator",
+    "project_meanfield",
+    "project_operator_to_m_body",
+    "project_to_n_body_operator",
+]
