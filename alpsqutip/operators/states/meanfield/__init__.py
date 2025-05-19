0ab5d57092a2f77e43b778a688b05808e80999d9
mmatera <matera@fisica.unlp.edu.ar>
2025-05-19 07:25:54 -0300

    black

--- alpsqutip/operators/states/meanfield/__init__.py
+++ alpsqutip/operators/states/meanfield/__init__.py
@@ -1,15 +1,13 @@
-
-
+from .meanfield import project_meanfield
 from .projections import (
     one_body_from_qutip_operator,
     project_operator_to_m_body,
-    project_to_n_body_operator
+    project_to_n_body_operator,
 )
-from .meanfield import project_meanfield
 
-ALL=[
+ALL = [
     "one_body_from_qutip_operator",
     "project_meanfield",
     "project_operator_to_m_body",
-    "project_to_n_body_operator"
+    "project_to_n_body_operator",
 ]
