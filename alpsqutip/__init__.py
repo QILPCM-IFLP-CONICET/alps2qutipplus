8f3b457f5738255718238ad36ccc942eb6c1a0a3
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-05-21 06:52:48 -0300

    removing root from imports in __init__

--- alpsqutip/__init__.py
+++ alpsqutip/__init__.py
@@ -6,14 +6,14 @@ Created on Tue Sep 12 19:07:05 2023
 @author: mauricio
 """
 
-import alpsqutip.geometry as geometry
-import alpsqutip.model as model
-import alpsqutip.operators as operators
-import alpsqutip.restricted_maxent_toolkit
-import alpsqutip.utils as utils
-from alpsqutip.alpsmodels import list_models_in_alps_xml, model_from_alps_xml
-from alpsqutip.geometry import graph_from_alps_xml, list_geometries_in_alps_xml
-from alpsqutip.model import build_system
+import .geometry as geometry
+import .model as model
+import .operators as operators
+import .restricted_maxent_toolkit
+import .utils as utils
+from .alpsmodels import list_models_in_alps_xml, model_from_alps_xml
+from .geometry import graph_from_alps_xml, list_geometries_in_alps_xml
+from .model import build_system
 
 __all__ = [
     "alpsqutip",
