7491aea94ed4b39d4866928aa1d72207f74601d4
mmatera <mmatera@fisica.unlp.edu.ar>
2025-05-30 17:01:22 -0300

    improving coordinate handling

--- alpsqutip/geometry.py
+++ alpsqutip/geometry.py
@@ -47,7 +47,7 @@ def graph_from_alps_xml(
 
     def process_coordinates(text: Optional[str] = None) -> Optional[list]:
         if text:
-            coords_expr = [for c in text.split(" ") if c!=""]
+            coords_expr = [c for c in text.split(" ") if c!=""]
             try:
                 return [eval_expr(c.strip(), parms) for c in coords_expr]
             except SyntaxError:
