dd549289959576c2f3e150b3c8cbc049b3fcf1ac
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-06-20 22:34:08 -0300

    OperatorBasis (#67)
    * using a class to handle basis
    
    * HierarchicalBasis
    
    * more typing annotations
    
    
    
    ---------
    
    Co-authored-by: mmatera <mmatera@fisica.unlp.edu.ar>

--- alpsqutip/evolution/maxent_evol.py
+++ alpsqutip/evolution/maxent_evol.py
@@ -4,19 +4,16 @@ Functions used to run MaxEnt simulations.
 
 from __future__ import annotations
 
+import logging
 from typing import List
 
-import numpy as np
-from scipy import linalg
-
-from alpsqutip.evolution.hierarchical_basis import (
-    build_hierarchical_basis,
-    fn_hij_tensor_with_errors,
-    k_state_from_phi_basis,
-)
 from alpsqutip.operators import Operator
-from alpsqutip.operators.states.meanfield.projections import project_to_n_body_operator
-from alpsqutip.scalarprod import fetch_covar_scalar_product, orthogonalize_basis
+from alpsqutip.operators.states import GibbsProductDensityOperator
+from alpsqutip.operators.states.meanfield import (
+    project_meanfield,
+    project_to_n_body_operator,
+)
+from alpsqutip.scalarprod import HierarchicalOperatorBasis, fetch_covar_scalar_product
 
 # function used to safely and robustly map K-states to states
 
@@ -56,20 +53,111 @@ def projected_evolution(
         A list with the solution at times t_span
 
     """
-    h_basis = build_hierarchical_basis(ham, k0, order)
     sp = fetch_covar_scalar_product(sigma_0)
-    # Project to n_body subspace if required
-    if n_body >= 0:
-        h_basis = [
-            project_to_n_body_operator(op_b, nmax=n_body, sigma=sigma_0)
-            for op_b in h_basis
-        ]
-    h_basis = orthogonalize_basis(h_basis, sp)
-    hij, werrs = fn_hij_tensor_with_errors(h_basis, sp, ham)
-    result = []
-    phi0 = np.array([sp(b_op, k0) for b_op in h_basis])
-    for indx, t in enumerate(t_span):
-        phi = linalg.expm(hij * t) @ phi0
-        k_inst = k_state_from_phi_basis(phi, h_basis)
-        result.append(k_inst)
-    return result
+
+    basis = HierarchicalOperatorBasis(
+        k0,
+        ham,
+        order,
+        sp,
+        n_body_projection=lambda op_b: project_to_n_body_operator(
+            op_b, nmax=n_body, sigma=sigma_0
+        ),
+    )
+
+    phi_0 = basis.coefficient_expansion(k0)
+    return [basis.operator_from_coefficients(basis.evolve(t, phi_0)[0]) for t in t_span]
+
+
+def adaptative_projected_evolution(
+    ham, k0, t_span, order, sigma_0, n_body: int = -1, tol=1e-3
+) -> List[Operator]:
+    """
+    Compute the solution of the MaxEnt projected Schrödinger equation
+
+    dk
+    -- = -i [H, k]
+    dt
+
+    as a linear combination of a an operator basis
+
+    k = sum phi_a(t) Q_a
+
+    chosen adaptatively along the evolution.
+
+    Parameters
+    ----------
+    ham : Operator
+        The Hamiltonian operator
+    k0 : Operator
+        The initial condition
+    t_span: np.array
+        the times for with the evolution is computed
+    order:
+        the order of the solution
+
+    n_body: int
+        if non-negative, build a solution projected on
+        the subspace of n_body operators.
+
+    tol: the maximum induced distance between the projected solution
+         and the exact solution.
+
+    Returns
+    -------
+    List[Operator]:
+        A list with the solution at times t_span
+
+    """
+
+    def update_basis(k, sigma):
+        k_ref_new, sigma = project_meanfield(
+            k, sigma, proj_function=project_to_n_body_operator
+        )
+        return (
+            HierarchicalOperatorBasis(
+                k,
+                ham,
+                order,
+                fetch_covar_scalar_product(sigma),
+                n_body_projection=lambda op_b: project_to_n_body_operator(
+                    op_b, nmax=n_body, sigma=sigma
+                ),
+            ),
+            sigma,
+        )
+
+    t_max = t_span[-1]
+    max_error_speed = tol / t_max
+
+    basis = update_basis(k0, sigma_0)
+    phi_0 = basis.coefficient_expansion(k0)
+    result = [k0]
+
+    k_t = k0
+    sigma_ref = GibbsProductDensityOperator(k_t)
+    last_t = t_ref = 0
+    for t in t_span[1:]:
+        delta_t = t - t_ref
+        phi, error = basis.evolve(delta_t, phi_0)
+        if error > max_error_speed * delta_t:
+            logging.info(
+                (
+                    f"At time {t} the estimated error {error} "
+                    f"is beyond the expected limit{max_error_speed * delta_t}. Updating basis."
+                )
+            )
+            basis, sigma_ref = update_basis(k_t, sigma_ref)
+            t_ref = last_t
+            delta_t = t - t_ref
+            phi, error = basis.evolve(delta_t, phi_0)
+            if error > max_error_speed * delta_t:
+                logging.warning(
+                    "tolerance goal cannot be reached within this subspace."
+                )
+                return result
+        k_t = basis.operator_from_coefficients(phi)
+        result.append(k_t)
+        last_t = t
+
+    return [basis.operator_from_coefficients([0]) for t in t_span]
