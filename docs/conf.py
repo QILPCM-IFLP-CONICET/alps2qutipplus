d9103b0cfcfd1c0137d8d3a5248b59b4ef5efe2d
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-06-30 09:41:14 -0300

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,47 +2,48 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = 'alps2qutipplus'
-copyright = '2025, QILPCM-IFLP-CONICET'
-author = 'QILPCM-IFLP-CONICET'
-release = '0.1.0'
+project = "alps2qutipplus"
+copyright = "2025, QILPCM-IFLP-CONICET"
+author = "QILPCM-IFLP-CONICET"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autosummary",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 # -- Autodoc settings --------------------------------------------------------
 
-autoclass_content = 'both'
-autodoc_member_order = 'bysource'
+autoclass_content = "both"
+autodoc_member_order = "bysource"
 
 # -- Napoleon settings (for Google/NumPy docstrings) ------------------------
 
