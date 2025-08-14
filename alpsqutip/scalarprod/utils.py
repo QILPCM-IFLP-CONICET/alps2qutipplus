a1c066470a96f7f1761a5c7e31601c9388619701
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-08-14 13:09:21 -0300

    fix append and prepend basis (#110)

--- alpsqutip/scalarprod/utils.py
+++ alpsqutip/scalarprod/utils.py
@@ -4,12 +4,17 @@ import numpy as np
 from numpy.typing import NDArray
 from scipy.linalg import qr
 
+from alpsqutip.settings import ALPSQUTIP_TOLERANCE
 
-def find_linearly_independent_rows(mat: NDArray, tol: float = 1e-6) -> Tuple[int]:
+
+def find_linearly_independent_rows(
+    mat: NDArray, tol: float = ALPSQUTIP_TOLERANCE
+) -> Tuple[int]:
     """
     Find indices of a maximal subset of linearly independent columns of the matrix.
     """
-    _, inds = qr(mat, mode="r", pivoting=True)
-    rank = np.linalg.matrix_rank(mat, tol=tol)
+    tol = min(tol, min(row[i] for i, row in enumerate(mat)) * 0.25)
+    r, inds = qr(mat, mode="r", pivoting=True)
+    rank = np.linalg.matrix_rank(r, tol=tol)
     # The first `rank` indices are linearly independent columns
     return tuple(sorted(inds[:rank]))
