5a09fc30518d8cc78d0b490a22eb060f29512335
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-09-02 15:33:18 -0300

    adjust tolerance for tests

--- test/helper.py
+++ test/helper.py
@@ -42,7 +42,7 @@ def sz_parity_projection(state):
     return project_parity_like(state, "Parity")
 
 
-EXPECTATION_VALUE_TOLERANCE = 1.0e-3  # 2e-1 # 1.0e-3
+EXPECTATION_VALUE_TOLERANCE = 2.0e-2  # 1.0e-3
 RELATIVE_ENTROPY_TOLERANCE = 8.0e-2  # 1.0e-6  # 8e-2  # 1.0e-6
 
 
